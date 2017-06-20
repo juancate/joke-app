f29c7bb0038f3497d22e52586b9d085618023d52
Julian Camargo <jcamargo2@gmail.com>
2017-06-19 20:13:36 -0500

    Add view encapsulation to joke form

--- src/app/joke-form/joke-form.component.ts
+++ src/app/joke-form/joke-form.component.ts
@@ -1,10 +1,11 @@
 import { Joke } from '../joke';
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-joke-form',
   templateUrl: './joke-form.component.html',
-  styleUrls: ['./joke-form.component.css']
+  styleUrls: ['./joke-form.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class JokeFormComponent {
     @Output() jokeCreated = new EventEmitter<Joke>();
