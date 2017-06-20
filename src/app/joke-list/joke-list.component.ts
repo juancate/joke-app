ca7769cee1e2f3f116fde298c984ac2e17a640e6
Julian Camargo <jcamargo2@gmail.com>
2017-06-19 23:26:45 -0500

    Use ViewChild to find child views

--- src/app/joke-list/joke-list.component.ts
+++ src/app/joke-list/joke-list.component.ts
@@ -1,19 +1,24 @@
 import { Joke } from '../joke';
-import { Component } from '@angular/core';
+import { JokeComponent } from '../joke/joke.component'
+import { Component, ViewChild } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-joke-list',
   templateUrl: './joke-list.component.html',
   styleUrls: ['./joke-list.component.css']
 })
-export class JokeListComponent {
+export class JokeListComponent implements AfterViewInit {
     jokes: Joke[];
+
+    @ViewChild(JokeComponent) jokeViewChild: JokeComponent;
 
     constructor() {
         this.jokes = [
             new Joke("What did the cheese say when it looked in the mirror?", "Hello-Me (Halloumi)"),
             new Joke("What kind of cheese do you use to disguise a small horse?", "Mask-a-pony (Mascarpone)"),
         ];
+        console.log(`new - jokeViewChild is ${this.jokeViewChild}`);
     }
 
     addJoke(joke: Joke) {
@@ -25,5 +30,9 @@ export class JokeListComponent {
         if (indexToDelete !== -1) {
             this.jokes.splice(indexToDelete, 1);
         }
+    }
+
+    ngAfterViewInit() {
+        console.log(`new - jokeViewChild is ${this.jokeViewChild}`);
     }
 }
